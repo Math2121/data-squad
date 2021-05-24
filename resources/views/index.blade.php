13bb254e199ed423431575f9b52eee30385641ad
Math2121 <matheusdepaula527@gmail.com>
2021-05-24 14:38:49 -0300

    insert text

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -394,10 +394,7 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <iframe width="100%" height="400" src="https://www.youtube.com/embed/1Um_7l9O_cY"
-                        title="YouTube video player" frameborder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen></iframe>
+                    <iframe width="100%" height="400" src="https://www.youtube.com/embed/_df79tBA8Ko" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
 
             </div>
